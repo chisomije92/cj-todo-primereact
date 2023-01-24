c76bb173e272f4c830b9ab2145baebc6b3fe8621
chisomije92 <chisomije92@gmail.com>
2023-01-24 09:45:36 +0100

    revert to placeholder json domain

--- src/components/services/TasksService.ts
+++ src/components/services/TasksService.ts
@@ -1,19 +1,19 @@
 
 export class TasksService {
   getTasks() {
-    return fetch('https://db-json-server.vercel.app/todos?_sort=id&_order=desc')
+    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos?_sort=id&_order=desc')
       .then(res => res.json())
   }
 
   deleteTask(id: number) {
-    fetch(`https://db-json-server.vercel.app/todos/${id}`, {
+    fetch(`https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos/${id}`, {
       method: 'DELETE',
     })
       .then(res => res.json())
   }
 
   createTask(task: any) {
-    return fetch('https://db-json-server.vercel.app/todos', {
+    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
