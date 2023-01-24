dae9b2bcaa884f6dcc037979300d6a5e1c5f0e61
chisomije92 <chisomije92@gmail.com>
2023-01-24 19:44:10 +0100

    add glitch json server domain

--- src/components/services/TasksService.ts
+++ src/components/services/TasksService.ts
@@ -1,19 +1,19 @@
 
 export class TasksService {
   getTasks() {
-    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos?_sort=id&_order=desc')
+    return fetch('https://heartbreaking-linen-angora.glitch.me/todos?_sort=id&_order=desc')
       .then(res => res.json())
   }
 
   deleteTask(id: number) {
-    fetch(`https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos/${id}`, {
+    fetch(`https://heartbreaking-linen-angora.glitch.me/todos/${id}`, {
       method: 'DELETE',
     })
       .then(res => res.json())
   }
 
   createTask(task: any) {
-    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos', {
+    return fetch('https://heartbreaking-linen-angora.glitch.me/todos', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
