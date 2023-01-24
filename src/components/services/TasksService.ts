1716fc3259449ecfd55386d9fad410c93a14c8a5
chisomije92 <chisomije92@gmail.com>
2023-01-24 09:02:12 +0100

    replace localhost domain with placeholder domain for json server

--- src/components/services/TasksService.ts
+++ src/components/services/TasksService.ts
@@ -1,29 +1,19 @@
 
 export class TasksService {
   getTasks() {
-    return fetch('http://localhost:3003/todos?_sort=id&_order=desc')
+    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos?_sort=id&_order=desc')
       .then(res => res.json())
   }
 
   deleteTask(id: number) {
-    fetch(`http://localhost:3003/todos/${id}`, {
+    fetch(`https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos/${id}`, {
       method: 'DELETE',
     })
       .then(res => res.json())
   }
 
-  replaceTasks(tasks: any) {
-    return fetch('http://localhost:3003/todos', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        ...tasks
-      })
-    }).then(res => res.json())
-  }
-
   createTask(task: any) {
-    return fetch('http://localhost:3003/todos', {
+    return fetch('https://my-json-server.typicode.com/chisomije92/cj-todo-primereact/todos', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
